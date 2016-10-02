913460e8d8bb236c5cb03d8808690820a9792950
felixacdc <pfmata01@gmail.com>
2016-10-02 16:05:23 -0600

    [add] frontend chat :metal:

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    server = require("http").createServer(app),
+    io = require("socket.io").listen(server);
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.on('sendMessage', function (data) {
+        io.socket.emit("newMessage", {msg: data});
+    });
+});
+
+server.listen(8080, function () {
+    console.log('Servidor corriendo en el puerto 8080');
+});
