810dd300ef7c8a84ec1cf72dc24d20b60b7e0eff
felixacdc <pfmata01@gmail.com>
2016-10-02 22:36:08 -0600

    [modify] finished second video :heart_eyes:

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
     app = express(),
     server = require("http").createServer(app),
-    io = require("socket.io").listen(server);
+    io = require("socket.io").listen(server),
+    usernames = {};
 
 app.use(express.static('public'));
 
@@ -11,8 +12,29 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
     socket.on('sendMessage', function (data) {
-        io.sockets.emit("newMessage", {msg: data});
+        io.sockets.emit("newMessage", {msg: data, user: socket.usernames});
     });
+    
+    socket.on('newUser', function(data, callback) {
+        if( data in usernames ) 
+            callback(false);
+        else {
+            callback(true);
+            socket.usernames = data;
+            usernames[socket.usernames] = 1;
+            updateUsernames();
+        }
+    });
+    
+    socket.on("disconnect", function (data) {
+        if(!socket.usernames) return;
+        delete usernames[socket.usernames];
+        updateUsernames();
+    });
+    
+    function updateUsernames () {
+        io.sockets.emit('usernames', usernames);
+    }
 });
 
 server.listen(8080, function () {
--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,23 @@ $(document).ready(function () {
         $messageForm = $('#sendMessage'),
         $message = $('#message'),
         $chat = $("#chat"),
+        $users = $("#users"),
+        $user = $('#meUser'),
+        $setUser = $("#btnUser"),
         scroll = 100;
+    
+    $setUser.click(function (e) {
+       e.preventDefault();
+        socket.emit('newUser', $user.val(), function (data) {
+            if( data ) {
+                $("#chatContent .panel-heading").append(` ${$user.val()}`);
+                $("#userContent").hide();
+                $("#content").show();
+            } else {
+                $("#login-error").show();
+            }
+        });
+    });
     
     $messageForm.submit(function (e) {
         e.preventDefault();
@@ -17,8 +33,16 @@ $(document).ready(function () {
     });
     
     socket.on('newMessage', function (data) {
-        $chat.append(` - ${data.msg} <br/>`);
+        $chat.append(`<strong>${data.user}</strong> - ${data.msg} <br/>`);
         scroll = scroll + 100;
         $chat.scrollTop(scroll);
+    });
+    
+    socket.on('usernames', function(data) {
+       var usernamesString = "";
+        for(var username in data) {
+            usernamesString += `${username} <br/>`;
+        }
+        $users.html(usernamesString);
     });
 });
